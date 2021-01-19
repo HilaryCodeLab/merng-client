60b709b572e9ec0f8bc37fd97ed4a8d803fcb785
Hilary_not_Hillary <hilarysoong@gmail.com>
2021-01-19 22:02:14 +0800

    added proxy and changed server base uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks';
 import {setContext} from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://nameless-reaches-40521.herokuapp.com/'
 });
 
 const authLink = setContext(()=>{
